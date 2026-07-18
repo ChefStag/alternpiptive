b1b0f6b121a97f10937fa9813de6d0947e7eda8e
ChefStag <stagchef@gmail.com>
2026-07-18 01:32:35 -0500

    Update intercepipted.sh
    Added code to the script

--- lib/intercepipted.sh
+++ lib/intercepipted.sh
@@ -1,1 +1,15 @@
+# intercepipted.sh - The Security Gatekeeper
+# Source this in your .bashrc
 
+pip() {
+    # 1. Provide the visual warning
+    echo -e "\033[1;31m!!! DANGER INTERCEPIPTED !!!\033[0m"
+    echo -e "\033[0;32mRedirecting Termux to a safer alternpiptive solution!\033[0m"
+    
+    # 2. Call the alternpiptive binary directly
+    # Assumes it was installed to $HOME/.alternpiptive/bin/
+    "$HOME/.alternpiptive/bin/alternpiptive" install "$2"
+}
+
+# Export the function so subshells inherit the protection
+export -f pip
